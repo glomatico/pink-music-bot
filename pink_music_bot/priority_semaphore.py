c21df3de001db5a136a3e359349721ee494ebc1e
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-05 01:00:13 -0300

    Make PrioritySemaphore thread-safe with asyncio.Lock

--- pink_music_bot/priority_semaphore.py
+++ pink_music_bot/priority_semaphore.py
@@ -17,31 +17,42 @@ class PrioritySemaphore:
         self._count = 0
         self._sequence = 0
         self._queue: asyncio.PriorityQueue[PriorityItem] = asyncio.PriorityQueue()
+        self._lock = asyncio.Lock()
 
     @asynccontextmanager
     async def acquire(self, priority: int = 0):
-        if self._count < self._limit:
-            self._count += 1
-            try:
-                yield
-            finally:
-                self._release()
-            return
+        async with self._lock:
+            if self._count < self._limit:
+                self._count += 1
+                acquired_immediately = True
+            else:
+                acquired_immediately = False
+                event = asyncio.Event()
+                self._sequence += 1
+                item = PriorityItem(
+                    priority=priority, sequence=self._sequence, event=event
+                )
+                await self._queue.put(item)
 
-        event = asyncio.Event()
-        self._sequence += 1
-        item = PriorityItem(priority=priority, sequence=self._sequence, event=event)
-        await self._queue.put(item)
-        await event.wait()
+        if not acquired_immediately:
+            try:
+                await event.wait()
+            except asyncio.CancelledError:
+                # If cancelled while waiting, check if we were already signaled
+                if event.is_set():
+                    # We got the semaphore, so we need to release it
+                    await self._release()
+                raise
 
         try:
             yield
         finally:
-            self._release()
+            await self._release()
 
-    def _release(self):
-        self._count -= 1
-        if not self._queue.empty():
-            self._count += 1
-            item = self._queue.get_nowait()
-            item.event.set()
+    async def _release(self):
+        async with self._lock:
+            self._count -= 1
+            if not self._queue.empty():
+                self._count += 1
+                item = self._queue.get_nowait()
+                item.event.set()
